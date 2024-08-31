59691548f94d52f84701c9cd4ba384fcef93f247
Oleg <olevole@home.olevole.ru>
2024-08-31 18:05:27 +0300

    sync

--- initforms.sh
+++ initforms.sh
@@ -13,7 +13,7 @@ fi
 
 set -e
 . ${distdir}/cbsd.conf
-. ${distdir}/tools.subr
+. ${subrdir}/tools.subr
 . ${subr}
 set +e
 
@@ -26,36 +26,36 @@ MYPATH="${workdir}/formfile"
 
 /usr/local/bin/sqlite3 ${MYPATH}/${HELPER}.sqlite << EOF
 BEGIN TRANSACTION;
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,1,"-","PHP Settings",'PHP Settings','PP','',1, "maxlen=60", "delimer", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,2,"php_version","php_version: 8.3, 8.3 , ..",'3','3','',1, "maxlen=60", "select", "php_version_select", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,3,"expose_php","expose_php: default is Off",'2','2','',1, "maxlen=60", "radio", "expose_php_onoff", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,4,"short_open_tag","default is Off",'Off','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,5,"realpath_cache_ttl","default is 120",'120','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"max_execution_time","default is 30",'30','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"max_input_time","default is 60",'60','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"memory_limit","default is 128M",'128M','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"cli_memory_limit","default is 512M",'512M','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"error_reporting","default is E_ALL",'E_ALL','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"display_errors","display_errors: default is On",'1','1','',1, "maxlen=60", "radio", "display_errors_onoff", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"error_log","default is /var/log/httpd/php.err",'/var/log/httpd/php.err','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"post_max_size","default is 8M",'8M','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"default_charset","default is UTF-8",'UTF-8','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"file_uploads","default is On",'On','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"upload_tmp_dir","default is /tmp",'/tmp','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"upload_max_filesize","default is 2M",'2M','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"date_timezone","default is Europe/Moscow",'Europe/Moscow','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"session_save_handler","default is files",'files','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"session_save_path","default is /tmp",'/tmp','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"opcache_enable","default is 1",'1','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"opcache_enable_cli","default is 1",'1','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"opcache_memory_consumption","default is 64",'64','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,7,"-","PHP-FPM Settings",'-','-','',1, "maxlen=60", "delimer", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,8,"fpm_user","default is www",'www','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,9,"fpm_group","default is www",'www','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,10,"pm_max_children","max php-fpm process. default is 8",'8','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,11,"log_level","default is error",'error','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,12,"fpm_error_log","default is /var/log/php-fpm.log",'/var/log/php-fpm.log','','',1, "maxlen=60", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,13,"fpm_listen","/tmp/php-fpm.sock",'/tmp/php-fpm.sock','','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,1,'-','PHP Settings','PHP Settings','PP','',1, 'maxlen=60', 'delimer', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,2,'php_version','php_version: 8.3, 8.3 , ..','3','3','',1, 'maxlen=60', 'select', 'php_version_select', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,3,'expose_php','expose_php: default is Off','2','2','',1, 'maxlen=60', 'radio', 'expose_php_onoff', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,4,'short_open_tag','default is Off','Off','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,5,'realpath_cache_ttl','default is 120','120','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'max_execution_time','default is 30','30','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'max_input_time','default is 60','60','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'memory_limit','default is 128M','128M','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'cli_memory_limit','default is 512M','512M','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'error_reporting','default is E_ALL','E_ALL','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'display_errors','display_errors: default is On','1','1','',1, 'maxlen=60', 'radio', 'display_errors_onoff', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'error_log','default is /var/log/httpd/php.err','/var/log/httpd/php.err','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'post_max_size','default is 8M','8M','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'default_charset','default is UTF-8','UTF-8','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'file_uploads','default is On','On','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'upload_tmp_dir','default is /tmp','/tmp','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'upload_max_filesize','default is 2M','2M','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'date_timezone','default is Europe/Moscow','Europe/Moscow','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'session_save_handler','default is files','files','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'session_save_path','default is /tmp','/tmp','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'opcache_enable','default is 1','1','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'opcache_enable_cli','default is 1','1','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,6,'opcache_memory_consumption','default is 64','64','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,7,'-','PHP-FPM Settings','-','-','',1, 'maxlen=60', 'delimer', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,8,'fpm_user','default is www','www','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,9,'fpm_group','default is www','www','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,10,'pm_max_children','max php-fpm process. default is 8','8','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,11,'log_level','default is error','error','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,12,'fpm_error_log','default is /var/log/php-fpm.log','/var/log/php-fpm.log','','',1, 'maxlen=60', 'inputbox', '', '' );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( 'forms', 1,13,'fpm_listen','/tmp/php-fpm.sock','/tmp/php-fpm.sock','','',1, 'maxlen=60', 'inputbox', '', '' );
 COMMIT;
 EOF
 
@@ -70,32 +70,32 @@ EOF
 # Put boolean for php_version_select
 /usr/local/bin/sqlite3 ${MYPATH}/${HELPER}.sqlite << EOF
 BEGIN TRANSACTION;
-INSERT INTO php_version_select ( text, order_id ) VALUES ( "8.2", 1 );
-INSERT INTO php_version_select ( text, order_id ) VALUES ( "8.3", 2 );
-INSERT INTO php_version_select ( text, order_id ) VALUES ( "8.4", 3 );
+INSERT INTO php_version_select ( text, order_id ) VALUES ( '8.2', 1 );
+INSERT INTO php_version_select ( text, order_id ) VALUES ( '8.3', 2 );
+INSERT INTO php_version_select ( text, order_id ) VALUES ( '8.4', 3 );
 COMMIT;
 EOF
 
 # Put boolean for expose_php_onoff
 /usr/local/bin/sqlite3 ${MYPATH}/${HELPER}.sqlite << EOF
 BEGIN TRANSACTION;
-INSERT INTO expose_php_onoff ( text, order_id ) VALUES ( "on", 1 );
-INSERT INTO expose_php_onoff ( text, order_id ) VALUES ( "off", 0 );
+INSERT INTO expose_php_onoff ( text, order_id ) VALUES ( 'on', 1 );
+INSERT INTO expose_php_onoff ( text, order_id ) VALUES ( 'off', 0 );
 COMMIT;
 EOF
 
 # Put boolean for display_errors_onoff
 /usr/local/bin/sqlite3 ${MYPATH}/${HELPER}.sqlite << EOF
 BEGIN TRANSACTION;
-INSERT INTO display_errors_onoff ( text, order_id ) VALUES ( "on", 1 );
-INSERT INTO display_errors_onoff ( text, order_id ) VALUES ( "off", 0 );
+INSERT INTO display_errors_onoff ( text, order_id ) VALUES ( 'on', 1 );
+INSERT INTO display_errors_onoff ( text, order_id ) VALUES ( 'off', 0 );
 COMMIT;
 EOF
 
 
 /usr/local/bin/sqlite3 ${MYPATH}/${HELPER}.sqlite << EOF
 BEGIN TRANSACTION;
-INSERT INTO system ( helpername, version, packages, have_restart ) VALUES ( "php", "201607", "lang/php73", "" );
+INSERT INTO system ( helpername, version, packages, have_restart ) VALUES ( 'php', '201607', 'lang/php73', '' );
 COMMIT;
 EOF
 
